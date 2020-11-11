dc36ebc85eeb5306aa25a1fdb13c7e1d0912e9a0
John Tourtellott <johnt@raytechsystems.com>
2020-11-10 22:11:46 -0500

    Fix "Reference Error: process not defined"
    Add plugin-replace and use it to set process.env.NODE_ENV

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,5 +1,6 @@
 import commonjs from '@rollup/plugin-commonjs'; // Convert CommonJS modules to ES6
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
 import vue from 'rollup-plugin-vue'; // Handle .vue SFC files
 import buble from '@rollup/plugin-buble'; // Transpile/polyfill with reasonable br
 
@@ -12,6 +13,9 @@ export default {
   plugins: [
     nodeResolve(),
     commonjs(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    }),
     vue({
       css: true, // Dynamically inject css as a <style> tag
       compileTemplate: true, // Explicitly convert template to render function
